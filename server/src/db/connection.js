b5dc454bf185b7617dddc584bb27925eb2f5a37b
Birjusingh Thakur <124129897+BirjuThakur@users.noreply.github.com>
2023-08-18 13:26:11 +0530

    added database

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // const URI = process.env.DBCONNECT;
-   const URI = "mongodb://127.0.0.1:27017/newecomapp";
+   const URI = process.env.DBCONNECT;
 
 const connectDB = () =>{
     mongoose.connect(URI).then(()=>console.log("connection successfully"))
